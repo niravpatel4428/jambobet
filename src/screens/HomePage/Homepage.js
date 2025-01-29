0f8b00fbd441fcb73385b1a45e6ce617d0a2ef08
“hardik.zindex” <hardikgol.zindex@gmail.com>
2025-01-29 16:07:14 +0530

    allremove

--- src/screens/HomePage/Homepage.js
+++ src/screens/HomePage/Homepage.js
@@ -1,59 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Layout from '../../layout'
 import Slidersection from './Slider'
 import Dashboard from './Dashboard'
-import mobilelight from "../../images/mobilelight.svg";
-import mobiledark from "../../images/mobiledark.svg";
 
 const Homepage = () => {
-
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
-    const toggleTheme = () => {
-
-        // Toggle the dark mode theme
-        const newTheme = isDarkMode ? "light" : "dark";
-        localStorage.setItem("theme", newTheme);
-
-        // Apply the theme to the document element
-        document.documentElement.classList.toggle("dark", newTheme === "dark");
-
-        // Update the state
-        setIsDarkMode(!isDarkMode);
-    };
-
-    useEffect(() => {
-        // Check localStorage or OS preference on page load
-        const savedTheme = localStorage.getItem("theme");
-        if (savedTheme === "dark") {
-            setIsDarkMode(true);
-            document.documentElement.classList.add("dark");
-        } else if (savedTheme === "light") {
-            setIsDarkMode(false);
-            document.documentElement.classList.remove("dark");
-        } else {
-            // Handle default based on system preference
-            const prefersDarkMode = window.matchMedia(
-                "(prefers-color-scheme: dark)"
-            ).matches;
-            setIsDarkMode(prefersDarkMode);
-            document.documentElement.classList.toggle("dark", prefersDarkMode);
-        }
-    }, []);
 
     return (
         <>
             <Layout>
-                <button
-                    onClick={toggleTheme}
-                    className="fixed top-[250px] right-0 z-[999] max-sm:flex sm:hidden max-md:w-[40px] max-md:h-[40px] w-[48px] h-[50px] flex justify-center items-center p-[5px_5px_5px_7px] bg-mutedblue dark:bg-yellow rounded-[5px_0px_0px_5px]"
-                >
-                    <img
-                        src={isDarkMode ? mobiledark : mobilelight}
-                        alt={"Light Mode"}
-                        className=""
-                    />
-                </button>
                 <Slidersection />
                 <Dashboard />
             </Layout>
