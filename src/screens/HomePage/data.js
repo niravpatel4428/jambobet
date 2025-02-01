6ed126e13e81284db20cd169275d42f6b678f86e
yashzindex <yashzindex@gmail.com>
2025-02-01 16:28:05 +0530

    added changes in home

---
+++ src/screens/HomePage/data.js
@@ -0,0 +1,351 @@
+export const leaguesData = [
+    {
+      leagueTitle: "Premier League",
+      titleWrps: [
+        {
+          matches: [
+            {
+              team1: "Everton FC",
+              team2: "Aston Villa",
+              details: [
+                { text: "15/01 22:30", bold: true },
+                { text: "#8587", bold: false },
+                { text: "+159 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "3.50", colSpan: 4, highlight: false },
+                  { label: "X", value: "3.35", colSpan: 4, highlight: false },
+                  { label: "2", value: "2.20", colSpan: 4, highlight: true },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.67",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.35",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "1.32",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.82", colSpan: 6, highlight: false },
+                  { label: "no", value: "1.89", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+            {
+              team1: "Leicester City",
+              team2: "Crystal Palace",
+              details: [
+                { text: "15/01 22:30", bold: true },
+                { text: "#5550", bold: false },
+                { text: "+153 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "3.70", colSpan: 4, highlight: false },
+                  { label: "X", value: "3.75", colSpan: 4, highlight: false },
+                  { label: "2", value: "1.99", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.81",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.29",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "1.29",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.62", colSpan: 6, highlight: false },
+                  { label: "no", value: "2.16", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+            {
+              team1: "Newcastle United",
+              team2: "Wolverhampton Wanderers",
+              details: [
+                { text: "15/01 22:30", bold: true },
+                { text: "#6406", bold: false },
+                { text: "+153 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.42", colSpan: 4, highlight: false },
+                  { label: "X", value: "5.20", colSpan: 4, highlight: false },
+                  { label: "2", value: "7.00", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.13",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.17",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "2.80",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.71", colSpan: 6, highlight: false },
+                  { label: "no", value: "2.02", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+            {
+              team1: "Arsenal FC",
+              team2: "Tottenham Hotspur",
+              details: [
+                { text: "15/01 23:00", bold: true },
+                { text: "#2425", bold: false },
+                { text: "+159 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.38", colSpan: 4, highlight: false },
+                  { label: "X", value: "5.80", colSpan: 4, highlight: false },
+                  { label: "2", value: "7.20", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.12",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.16",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "2.95",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.64", colSpan: 6, highlight: false },
+                  { label: "no", value: "2.13", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      leagueTitle: "Italy / Serie A (1)",
+      titleWrps: [
+        {
+          matches: [
+            {
+              team1: "Inter Milano",
+              team2: "Bologna FC",
+              details: [
+                { text: "15/01 22:45", bold: true },
+                { text: "#4373", bold: false },
+                { text: "+153 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.5", colSpan: 4, highlight: false },
+                  { label: "X", value: "4.70", colSpan: 4, highlight: false },
+                  { label: "2", value: "6.60", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.14",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.21",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "2.50",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.88", colSpan: 6, highlight: false },
+                  { label: "no", value: "1.82", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      leagueTitle: "England / Championship (1)",
+      titleWrps: [
+        {
+          matches: [
+            {
+              team1: "Blackburn Rovers",
+              team2: "Portsmouth FC",
+              details: [
+                { text: "15/01 22:45", bold: true },
+                { text: "#4913", bold: false },
+                { text: "+153 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.70", colSpan: 4, highlight: false },
+                  { label: "X", value: "3.90", colSpan: 4, highlight: false },
+                  { label: "2", value: "5.00", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.19",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.27",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "2.11",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.85", colSpan: 6, highlight: false },
+                  { label: "no", value: "1.85", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+            {
+              team1: "Blackburn Rovers",
+              team2: "Portsmouth FC",
+              details: [
+                { text: "15/01 22:45", bold: true },
+                { text: "#4913", bold: false },
+                { text: "+153 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.70", colSpan: 4, highlight: false },
+                  { label: "X", value: "3.90", colSpan: 4, highlight: false },
+                  { label: "2", value: "5.00", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.19",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.27",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "2.11",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.85", colSpan: 6, highlight: false },
+                  { label: "no", value: "1.85", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+            {
+              team1: "Zalaegerszeg TE",
+              team2: "SK Lisen Brno",
+              details: [
+                { text: "STARTS IN • 39'", bold: true, colorRed: true },
+                { text: "#5756", bold: false },
+                { text: "+92 more", bold: false },
+              ],
+              odds: [
+                [
+                  { label: "1", value: "1.68", colSpan: 4, highlight: false },
+                  { label: "X", value: "3.85", colSpan: 4, highlight: true },
+                  { label: "2", value: "4.30", colSpan: 4, highlight: false },
+                ],
+                [
+                  {
+                    label: "1 or X",
+                    value: "1.20",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "1 or 2",
+                    value: "1.23",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                  {
+                    label: "X or 2",
+                    value: "1.97",
+                    colSpan: 4,
+                    highlight: false,
+                  },
+                ],
+                [
+                  { label: "yes", value: "1.68", colSpan: 6, highlight: false },
+                  { label: "no", value: "2.11", colSpan: 6, highlight: false },
+                ],
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+
